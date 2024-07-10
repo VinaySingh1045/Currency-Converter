74d7f83de54d3fda248b2f2e83ceef0b3070e2ae
Vinay Singh <157957914+VinaySingh1045@users.noreply.github.com>
2024-07-10 16:34:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,6 @@ function App() {
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
-        // backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
         backgroundColor: '#712323'
       }}
     >
