aa6b357bc884605e663dc367cfad4788b10884c7
Vinay Singh <157957914+VinaySingh1045@users.noreply.github.com>
2024-07-10 16:33:58 +0530

    Update index.js

--- src/compnents/index.js
+++ src/compnents/index.js
@@ -1,7 +1,3 @@
-// isko banate hai jab humare pass jada components ho jaye to hum usse bar bar import na kare ye direct isko kar ke use kar le 
-
 import InputBox from "./InputBox";
 
 export {InputBox}
-
-// like this way if we want to addd some etc just like this {InputBox,Button, etc...}
