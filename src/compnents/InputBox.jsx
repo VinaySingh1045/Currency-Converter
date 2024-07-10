a313dabbccb8e0e17318e7c3a5ccf807877564f7
VinaySingh1045 <vy0891547@gmail.com>
2024-07-10 16:31:17 +0530

    init

---
+++ src/compnents/InputBox.jsx
@@ -0,0 +1,59 @@
+import { useId } from "react";
+
+function InputBox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
+    className = "",
+})
+// yaha pe hum para de rahe hai
+{
+   const amountInputId = useId()
+//    ye humne bond karne ko banya hai taki ek jaise hi number rahe jaha bhi use kare waha pe eg me label or input ko dekho lo vaha use kiya hai likhne isski itne jarurat nahi hai hum mynum me de sakhte the dono me.
+
+    return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
+            <div className="w-1/2">
+                <label htmlFor={amountInputId} className="text-black/40 mb-2 inline-block">
+                    {label}
+                </label>
+                <input
+                    id={amountInputId}
+                    className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={amount}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                // yaha Number isliye kyuki ye string deta hai na
+                />
+            </div>
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+                >
+                    {
+                        currencyOptions.map((curr) => (
+
+                            <option key={curr} value={curr}>
+                                {curr}
+                            </option>
+                        ))
+                    }
+
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default InputBox;
---
+++ src/compnents/index.js
@@ -0,0 +1,7 @@
+// isko banate hai jab humare pass jada components ho jaye to hum usse bar bar import na kare ye direct isko kar ke use kar le 
+
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+// like this way if we want to addd some etc just like this {InputBox,Button, etc...}
